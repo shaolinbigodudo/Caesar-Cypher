ac3c147a416a4dfc9a419eaa918b96283d60102f
shaolinbigodudo <brunoedit234@gmail.com>
2024-06-13 09:46:27 -0300

    final

--- app/decode/__init__.py
+++ app/decode/__init__.py
@@ -1,5 +1,5 @@
-def encode_message(message, shift):
-    alphabet = ['A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ', '/r']
+def decode_message(message, shift):
+    alphabet = ['A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
     partialStart = ''
     partialEnd = ''
     shiftedAlphabet = ''
